86a6b0fc1692b83c048bf2a68a38406251f426fb
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-02-26 21:26:15 -0800

    Add SQL statements to generate contacts list

---
+++ gregsList.sql
@@ -0,0 +1,27 @@
+CREATE TABLE my_contacts
+( last_name VARCHAR(30)
+  , first_name VARCHAR(20)
+  , email VARCHAR(50)
+  , gender CHAR(1)
+  , birthday DATE
+  , profession VARCHAR(50)
+  , location VARCHAR(50)
+  , status VARCHAR(20)
+  , interests VARCHAR(100)
+  , seeking VARCHAR(100)
+);
+
+INSERT INTO my_contacts
+(last_name, first_name, email, gender, birthday, profession, location, status,
+  interests, seeking)
+VALUES
+('Anderson', 'Jillian', 'jill_anderson@breakneckpiza.com', 'F', '1980-09-05',
+  'Technical Writer', 'Palo Alto, CA', 'Single', 'Kayaking, Reptiles',
+  'Relationship, Friends');
+
+INSERT INTO my_contacts
+(first_name, email, profession, location)
+VALUES
+('Pat', 'patpost@breakneckpizza.com', 'Postal Worker', 'Princeton, NJ');
+
+
