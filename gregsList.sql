ae13d5fe590b594fe54fe042cc614974f172273e
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-03-02 20:50:11 -0800

    Add interests table to gregsList

--- gregsList.sql
+++ gregsList.sql
@@ -26,4 +26,11 @@ INSERT INTO my_contacts
 VALUES
 ('Pat', 'patpost@breakneckpizza.com', 'Postal Worker', 'Princeton, NJ');
 
-
+CREATE TABLE interests
+( int_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+  , interest VARCHAR(50) NOT NULL
+  , contact_id INT NOT NULL
+  , CONSTRAINT my_contacts_contact_id_fk
+  FOREIGN KEY (contact_id)
+  REFERENCES my_contacts (contact_id)
+);
